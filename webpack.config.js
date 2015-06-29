b25a261566275f0961be3cd45ea3ec08fb7dcaa5
caichao <hkongm@gmail.com>
2015-06-29 17:54:37 +0800

    add new file ext jsx for babel

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'react-hot!babel'
         // loader: 'react-hot!jsx-loader?harmony'
